1085493ad3f1898240a4807ae15a0963b77549d6
HSHSHSHSHSHSHSHSHSHS <157904084+HSHSHSHSHSHSHSHSHSHS@users.noreply.github.com>
2024-11-19 16:38:22 -0500

    Update ALLSANTA.R

--- ALLSANTA.R
+++ ALLSANTA.R
@@ -6,7 +6,7 @@ rm(list = ls())
 
 # prep --------------------------------------------------------------------
 
-og <- read_xlsx("/Users/weeb/Downloads/*data proj/Santa/MSAC Secret Santa Creative Fanwork Exchange 2024.xlsx")
+og <- read_xlsx("excel.xlsx")
 og <- og %>%
   rename(Discord_username = `Discord username`) %>%
   rename(`gift_give_type (Other)` = `gift_give_type (Other - Please specify)`)
@@ -380,10 +380,6 @@ match <- my_match
 
 match
 
-##
-# match <- read_xlsx("/Users/weeb/Downloads/*data proj/Santa/MSAC 2024 Secret Santa Tracker.xlsx", sheet = "Track")
-# match <- match[,c(1,3,6)]
-
 # Emails ------------------------------------------------------------------
 
 email_ctrl_c_ctrl_v_csv <- function(row) {
@@ -392,7 +388,7 @@ email_ctrl_c_ctrl_v_csv <- function(row) {
   print(name)
   if (name %in% email_csv[1]) next #no dupes
   
-  greet <- paste0("Hi ", name, ", this is an update on MSAC's Secret Santa Gift Exchange! 🎅 We have your giftee information ready:")
+  greet <- paste0("Hi ", name, ", this is an update on our Secret Santa Gift Exchange! 🎅 We have your giftee information ready:")
   
   just_matches <- match %>%
     filter (Santa == name)
@@ -473,7 +469,7 @@ email_ctrl_c_ctrl_v_csv <- function(row) {
   close <- paste0("\n\n\nAnd now you have all the info you need to get creating!! 🖊️ You only gotta make 1 \"thing\" per person, pick any character from their wishlist and do your best!",
   "\nAs a reminder you have until **December 21st to finish your gift(s)** *unless* you're creating a craft/physical piece, in which case you'll have until Dec 8th to finish. We'll **check in on Dec 6th** to make sure you feel like you're on track to have your gift ready and again on the 21st to confirm you're done. More info about how to share your creation with your giftee at checkin! 🎁",
   "\n\nThanks for participating, we're super excited to see what everyone whips up :D",
-  "\n\nWe know this was a lot,,,, have any questions, concerns, have you changed your username? Feel free to reach out to the person you dmed you this or by [contact form](<https://tally.so/r/m6QvW5>)",
+  "\n\nWe know this was a lot,,,, have any questions, concerns, have you changed your username? Feel free to reach out to the person you dmed you this or by email.",
   "\n\n# IMPORTANT:",
   "\nTo confirm you've received the info and you're still interested and available, please reply to this message with the name of your giftee(s) or react to this message with the first letter of their username(s) within 72 hours, otherwise we'll assume you can't participate and have dropped out and we'll go fix santa-giftee pairings'")
   
